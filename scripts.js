276800f9c70c7b63bd827a9fe38db0c02b3cd4fd
Brad C <bradcurry@gmail.com>
2022-05-05 08:31:14 -0700

    Add functions for playRound and userPlay

--- scripts.js
+++ scripts.js
@@ -1,14 +1,67 @@
 function computerPlay() {
     let number = Math.floor(Math.random() * 3);
-    let result = '';
+    let computerSelection = '';
     if (number === 0) {
-        result = 'rock';
+        computerSelection = 'Rock';
     } else if (number === 1) {
-        result = 'paper';
+        computerSelection = 'Paper';
     } else if (number === 2 ) {
-        result = 'scissors';
+        computerSelection = 'Scissors';
     }
-    console.log(result);
+    return computerSelection;
 }
 
 computerPlay();
+
+function userPlay() {
+    let playerSelection = prompt('Choose: (R)ock, (P)aper or (S)cissors')
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+        playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+        return playerSelection;
+    } else if (playerSelection === 'r' || playerSelection === 'p' || playerSelection === 's') {
+        if (playerSelection === 'r') {
+            playerSelection = 'Rock';
+        } else if (playerSelection === 'p') {
+            playerSelection = 'Paper';
+        } else if (playerSelection === 's') {
+            playerSelection = 'Scissors';
+        }
+        return playerSelection;
+    } else {
+        alert('Try again, must be (R)ock, (P)aper or (S)cissors');
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let computerWin = `You Lose! ${computerSelection} beats ${playerSelection}`
+
+    let playerWin = `You Win! ${playerSelection} beats ${computerSelection}`
+
+    if (playerSelection === computerSelection) {
+        return("It's a tie!")
+    } else if (playerSelection === 'Rock') {
+        if (computerSelection === 'Paper') {
+            return computerWin;
+        } else if (computerSelection === 'Scissors') {
+            return playerWin;
+        }
+    } else if (playerSelection === 'Paper') {
+        if (computerSelection === 'Rock') {
+            return playerWin;
+        } else if (computerSelection === 'Scissors') {
+            return computerWin;
+        }
+    } else if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Paper') {
+            return playerWin;
+        } else if (computerSelection === 'Rock') {
+            return computerWin;
+        }
+    }
+}
+
+const computerSelection = computerPlay();
+const playerSelection = userPlay();
+
+console.log(playRound(playerSelection, computerSelection));
